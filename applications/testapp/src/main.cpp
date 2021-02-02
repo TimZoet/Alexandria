f4e272128a765297ac4afd45075eeb190e0120ac
Tim Zoet <ts.zoet@gmail.com>
2021-02-02 13:40:05 +0100

    Updated submodule

--- applications/testapp/src/main.cpp
+++ applications/testapp/src/main.cpp
@@ -2,12 +2,6 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
-#include <bit>
-#include "cppql/binding.h"
-#include "cppql/column.h"
-#include "cppql/database.h"
-#include "cppql/statement.h"
-#include "cppql/table.h"
 
 #include "alexandria/library.h"
 
@@ -17,19 +11,51 @@ int main()
     std::filesystem::remove(dbPath);
 
     {
+        
+#if 1
         auto library = alex::Library::create(dbPath);
+        auto& float3_t   = library->createType("float3");
+        auto& material_t = library->createType("material");
+        auto& mesh_t     = library->createType("mesh");
+        auto& mat3x3_t   = library->createType("mat3x3");
+        auto& node_t     = library->createType("node");
 
-        auto& prop_x        = library->createProperty("x", alex::Property::DataType::Float, false, false);
-        auto& prop_y        = library->createProperty("y", alex::Property::DataType::Float, false, false);
-        auto& prop_z        = library->createProperty("z", alex::Property::DataType::Float, false, false);
-        auto& float3_t      = library->createType("float3", prop_x, prop_y, prop_z);
-        auto& prop_name     = library->createProperty("name", alex::Property::DataType::String, false, false);
-        auto& prop_albedo   = library->createProperty("albedo", float3_t, false, false);
-        auto& prop_specular = library->createProperty("specular", alex::Property::DataType::Float, false, false);
-        auto& prop_vertices = library->createProperty("vertices", float3_t, true, true);
-        auto& material_t    = library->createType("material", prop_name, prop_albedo, prop_specular);
-        auto& mesh_t        = library->createType("mesh", prop_name, prop_vertices);
+        auto& prop_x         = library->createPrimitiveProperty("x", alex::DataType::Float);
+        auto& prop_y         = library->createPrimitiveProperty("y", alex::DataType::Float);
+        auto& prop_z         = library->createPrimitiveProperty("z", alex::DataType::Float);
+        auto& prop_name      = library->createPrimitiveProperty("name", alex::DataType::String);
+        auto& prop_albedo    = library->createNestedProperty("albedo", float3_t);
+        auto& prop_specular  = library->createPrimitiveProperty("specular", alex::DataType::Float);
+        auto& prop_vertices  = library->createBlobProperty("vertices");
+        auto& prop_r1        = library->createNestedProperty("r1", float3_t);
+        auto& prop_r2        = library->createNestedProperty("r2", float3_t);
+        auto& prop_r3        = library->createNestedProperty("r3", float3_t);
+        auto& prop_transform = library->createNestedProperty("transform", mat3x3_t);
+        auto& prop_children  = library->createNestedArrayProperty("children", node_t);
+        auto& prop_meshes    = library->createNestedArrayProperty("meshes", mesh_t);
+        auto& prop_material  = library->createNestedProperty("material", material_t);
 
+        float3_t.addProperty(prop_x);
+        float3_t.addProperty(prop_y);
+        float3_t.addProperty(prop_z);
+        material_t.addProperty(prop_name);
+        material_t.addProperty(prop_albedo);
+        material_t.addProperty(prop_specular);
+        mesh_t.addProperty(prop_name);
+        mesh_t.addProperty(prop_vertices);
+        mat3x3_t.addProperty(prop_r1);
+        mat3x3_t.addProperty(prop_r2);
+        mat3x3_t.addProperty( prop_r3);
+        node_t.addProperty(prop_name);
+        node_t.addProperty(prop_transform);
+        node_t.addProperty(prop_children);
+        node_t.addProperty(prop_meshes);
+        node_t.addPropertyReference(prop_material);
+
+        library->commitTypes();
+#else
+        auto library = alex::Library::open(dbPath);
+#endif
         std::ofstream graph(std::filesystem::current_path() / "graph.dot");
         library->writeGraph(graph);
     }
