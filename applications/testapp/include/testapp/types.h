27bfe326a9d7f419c804f2632ec17f6d0ec1f29d
Tim Zoet <ts.zoet@gmail.com>
2021-04-01 19:34:25 +0200

    Updated submodules

--- applications/testapp/include/testapp/types.h
+++ applications/testapp/include/testapp/types.h
@@ -2,15 +2,21 @@
 
 #include "alexandria/library.h"
 
+struct Foo
+{
+    int32_t x = 0, y = 0;
+};
+
 struct Type0
 {
-    int64_t      id = 0;
-    alex::Blob<> blob;
-    float        f = 10;
+    int64_t                      id = 0;
+    float                        x  = 0;
+    int32_t                      y  = 0;
+    alex::Blob<std::vector<Foo>> foo;
 };
 
 struct Type1
 {
-    int64_t      id = 0;
-    alex::Blob<> blob;
+    int64_t                     id = 0;
+    alex::ReferenceArray<Type0> refs;
 };
--- applications/testapp/src/main.cpp
+++ applications/testapp/src/main.cpp
@@ -14,43 +14,47 @@ int main()
     std::filesystem::remove(dbPath);
 
     auto [library, created] = alex::Library::openOrCreate(dbPath);
-    alex::Type *type0;
+    alex::Type *type0, *type1;
     if (created)
     {
         type0 = &library->createType("type0");
-        //type1            = &library->createType("type1");
+        type1 = &library->createType("type1");
 
-        auto& blobProp = library->createBlobProperty("blob", false);
         auto& floatProp = library->createPrimitiveProperty("float", alex::DataType::Float);
-        type0->addProperty(blobProp);
+        auto& intProp   = library->createPrimitiveProperty("int", alex::DataType::Int32);
+        auto& blobProp   = library->createBlobProperty("blob", false);
+        auto& refProp   = library->createNestedArrayProperty("ref", *type0);
         type0->addProperty(floatProp);
+        type0->addProperty(intProp);
+        type0->addProperty(blobProp);
+        type1->addProperty(refProp);
         library->commitTypes();
     }
     else
     {
         type0 = &library->getType("type0");
-        //type1 = &library->getType("type1");
+        type1 = &library->getType("type1");
     }
 
-    auto handler0 = library->createObjectHandler<&Type0::id, &Type0::blob, &Type0::f>(type0->getName());
-
-    //auto handler1 = library->createObjectHandler<&Type1::id, &Type1::blob>(type1->getName());
-
-
+    auto handler0 = library->createObjectHandler<&Type0::id, &Type0::x, &Type0::y, &Type0::foo>(type0->getName());
+    auto handler1 = library->createObjectHandler<&Type1::id, &Type1::refs>(type1->getName());
 
     Type0 obj0;
-    obj0.blob.get().push_back(10);
-    obj0.blob.get().push_back(11);
-    obj0.blob.get().push_back(12);
-    obj0.blob.get().push_back(13);
+    obj0.x = 5.5f;
+    obj0.y = 10;
+    obj0.foo.get().emplace_back(3, 4);
     handler0.insert(obj0);
 
-    /*Type1 obj1;
-    obj1.blob.get().push_back(11);
-    obj1.blob.get().push_back(22);
-    obj1.blob.get().push_back(33);
-    handler1.insert(obj1);*/
+    Type0 obj1;
+    obj1.x = 3.5f;
+    obj1.y = 33;
+    handler0.insert(obj1);
 
-    //auto x = t0.get(2);
+    Type1 obj2;
+    obj2.refs.add(obj0);
+    obj2.refs.add(obj1);
+    handler1.insert(obj2);
+
+    auto x = handler0.get(obj0.id);
     std::cout << 'x';
 }
