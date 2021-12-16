2bce28e003d57abc2b930ddd9d996f4e1456bff7
Tim Zoet <ts.zoet@gmail.com>
2021-12-16 19:56:07 +0100

    Greatly improved packaging and conan integration

---
+++ conanfile.py
@@ -0,0 +1,87 @@
+from conans import ConanFile
+
+class AlexandriaConan(ConanFile):
+    ############################################################################
+    ## Package info.                                                          ##
+    ############################################################################
+    
+    name = "alexandria"
+    
+    description = "..."
+    
+    url = "https://github.com/TimZoet/Alexandria"
+
+    ############################################################################
+    ## Settings.                                                              ##
+    ############################################################################
+
+    python_requires = "pyreq/1.0.0@timzoet/stable"
+    
+    python_requires_extend = "pyreq.BaseConan"
+    
+    ############################################################################
+    ## Base methods.                                                          ##
+    ############################################################################
+    
+    def set_version(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        base.set_version(self, "modules/alexandria/alexandriaVersionString.cmake", "ALEXANDRIA_VERSION")
+    
+    def init(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        self.generators = base.generators + self.generators
+        self.settings = base.settings + self.settings
+        self.options = {**base.options, **self.options}
+        self.default_options = {**base.default_options, **self.default_options}
+    
+    ############################################################################
+    ## Building.                                                              ##
+    ############################################################################
+    
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        self.copy("license")
+        self.copy("readme.md")
+        self.copy("cmake/*")
+        self.copy("modules/CMakeLists.txt")
+        self.copy("modules/alexandria/*")
+    
+    def config_options(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+    
+    def requirements(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        base.requirements(self)
+        
+        self.requires("common/1.0.0@timzoet/stable")
+        self.requires("cppql/1.0.0@timzoet/stable")
+        self.requires("dot/1.0.0@timzoet/stable")
+        self.requires("fmt/7.0.3")
+
+        if self.options.build_tests:
+            self.requires("bettertest/1.0.0@timzoet/stable")
+    
+    def package_info(self):
+        self.cpp_info.libs = ["alexandria"]
+    
+    def generate(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        
+        tc = base.generate_toolchain(self)
+        tc.generate()
+        
+        deps = base.generate_deps(self)
+        deps.generate()
+
+    def build(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        cmake = base.configure_cmake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        cmake = base.configure_cmake(self)
+        cmake.install()
